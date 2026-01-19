b25af9ae64c2bc9bb82772749bfbf0daaa4d5495
longdc99 <long@myspa.vn>
2026-01-19 13:30:10 +0700

    [UPDATE] configs ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,13 +2,21 @@ module.exports = {
   apps: [
     {
       name: "content-writer-ui",
+      cwd: __dirname,
+
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3001",
-      cwd: __dirname,
-      instances: 1,
+
       exec_mode: "fork",
+      instances: 1,
+
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 1000,
+
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 3001
       }
     }
   ]
